db2c117aa8c990d7fb8afa530741813ad8d35f13
Arteloo <arhipovartem509@gmail.com>
2022-11-14 16:41:40 +0300

    First html-pages, mysql serv conn created, DB works

---
+++ app.js
@@ -0,0 +1,36 @@
+const mysql = require("mysql2");
+const express = require("express");
+ 
+const app = express();
+const urlencodedParser = express.urlencoded({extended: false});
+ 
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "DiplomaDB",
+  password: ""
+});
+connection.connect( function(err) {
+  if (err) {
+    return console.error("Ошибка: " + err.message);
+  }
+  else{
+    console.log("Подключение к серверу MySQL успешно установлено");
+  }
+app.set("view engine", "hbs");
+});
+// получение списка пользователей
+app.get("/", function(req, res){
+    connection.query("SELECT * FROM Machines", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index.hbs", {
+          Machines: data
+      });
+    });
+});
+app.get("/create", function(req, res){
+  res.render("create.hbs");
+});
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
